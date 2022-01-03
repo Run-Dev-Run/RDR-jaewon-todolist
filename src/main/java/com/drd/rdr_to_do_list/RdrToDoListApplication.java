690739bab4591ba26012efe831fd987aaf442ee5
darkant99 <darkant99@naver.com>
2022-01-03 12:53:21 +0900

    Init

---
+++ src/main/java/com/drd/rdr_to_do_list/RdrToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.drd.rdr_to_do_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdrToDoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RdrToDoListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/drd/rdr_to_do_list/RdrToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drd.rdr_to_do_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdrToDoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
