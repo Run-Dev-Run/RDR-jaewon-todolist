7123f397b8ec94cdfe67ecd3909fa50de412a43b
darkant99 <darkant99@naver.com>
2022-01-05 20:20:19 +0900

    [FEAT] Member 도메인과 스프링 시큐리티 삭제

--- src/main/java/com/drd/rdr_to_do_list/api/member/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.drd.rdr_to_do_list.api.member.repository;
-
-import com.drd.rdr_to_do_list.api.member.domain.entity.Member;
-
-import java.util.Optional;
-
-public interface MemberRepository {
-    Optional<Member> findByEmail(String email);
-}
